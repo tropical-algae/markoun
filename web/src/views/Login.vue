134f1f2cfde831dc61645191ce0e0344f67001b0
tropicalalgae <tropicalalgae@gmail.com>
2026-01-22 17:41:37 +0800

    feat: [site] add login page with form and styles

---
+++ web/src/views/Login.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+
+const loginForm = reactive({ username: "", password: "" })
+const registerForm = reactive({ 
+  email: "", 
+  username: "", 
+  password: "", 
+  confirmPassword: "" 
+})
+
+
+const onLogin = async () => {
+
+}
+
+const onRegister = async () => {
+  
+}
+</script>
+
+<template>
+  <div class="auth-wrapper d-flex flex-column justify-content-center align-items-center px-4 pb-4">
+		<div class="site-brand d-flex align-items-center">
+			MARKOUN.
+		</div>
+
+    <div class="auth-container d-flex flex-grow-1 justify-content-center align-items-center">
+      <div style="width: 100%; max-width: 480px; min-width: 320px;">
+
+				<h1 class="auth-title">Login</h1>
+				<form @submit.prevent="onLogin" class="auth-form">
+					
+					<div class="auth-input-wrapper">
+						<label class="input-label">Name</label>
+						<input 
+							v-model="loginForm.username" 
+							type="text" 
+							class="input-field"
+						/>
+					</div>
+
+					<div class="auth-input-wrapper mb-3">
+						<label class="input-label">Password</label>
+						<input 
+							v-model="loginForm.password" 
+							type="password" 
+							class="input-field"
+							placeholder=""
+						/>
+					</div>
+
+					<div class="links-row">
+						<a href="#">Forgot password ?</a>
+						<a href="#">Create an account</a>
+					</div>
+
+					<div class="d-flex justify-content-center">
+						<button type="submit" class="auth-submit-btn">
+							Sign in
+						</button>
+					</div>
+
+				</form>
+
+			</div>
+    </div>
+  </div>
+</template>
